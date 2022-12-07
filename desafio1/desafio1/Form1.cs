af9c99aef36442e1e4b54a6d3bd75c589671ba70
estresadinha <120022356+estresadinha@users.noreply.github.com>
2022-12-07 09:24:22 -0300

    Add files via upload

---
+++ desafio1/desafio1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text= "A cor é roxo";
+            label1.ForeColor = Color.Purple;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "A cor é verde";
+            label1.ForeColor = Color.Lime;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "A cor é vermelho";
+            label1.ForeColor = Color.Red;
+        }
+    }
+}
